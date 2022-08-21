9188450b4f2fc984c090683acff2ff87862f9ddc
Alex Ogden <alex.s.ogden@icloud.com>
2022-08-21 21:12:10 +0100

    Adding output dialogue to script

--- deploy-goconvert.sh
+++ deploy-goconvert.sh
@@ -6,13 +6,19 @@ scripts_directory="$HOME/.scripts/container-scripts"
 container_name="goconvert"
 script_name="30-start-$container_name.sh"
 
-# Build the new image with newest changes
+# Pull newest changes
+echo "LOG: Pulling latest changes"
 cd "$site_directory" || exit
 git pull
+
+# Build new image
+echo "LOG: Building new container image"
 docker build -t "$container_name" .
 
 # Remove currently running container
+echo "LOG: Removing currently running container"
 docker rm -f "$container_name"
 
 # Deploy the new container image
+echo "LOG: Deploying new container image"
 bash "$scripts_directory/$script_name"
