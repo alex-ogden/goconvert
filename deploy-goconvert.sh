c249eb8d22da4da2e520baecba82069a5196aa44
Alex Ogden <alex.s.ogden@icloud.com>
2022-08-23 19:57:06 +0000

    Cleaning the deployment script up a little bit

--- deploy-goconvert.sh
+++ deploy-goconvert.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-# Vars
+# General script variables
 site_directory="$HOME/sites/goconvert/"
-scripts_directory="$HOME/.scripts/container-scripts"
 container_name="goconvert"
+
+# My-server-specific vars
+scripts_directory="$HOME/.scripts/container-scripts"
 script_name="30-start-$container_name.sh"
 
 # Pull newest changes
-echo "LOG: Pulling latest changes"
-cd "$site_directory" || exit
-git pull
-
-# Build new image
-echo "LOG: Building new container image"
-docker build -t "$container_name" .
+pull_and_build() {
+	cd "$site_directory" || exit
+	git pull
+	docker build -t "$container_name" .
+}
 
 # Remove currently running container
-echo "LOG: Removing currently running container"
-docker rm -f "$container_name"
+clean_and_run() {
+	docker rm -f "$container_name"
+	bash "$scripts_directory/$script_name"
+}
 
-# Deploy the new container image
-echo "LOG: Deploying new container image"
-bash "$scripts_directory/$script_name"
+pull_and_build
+clean_and_run
